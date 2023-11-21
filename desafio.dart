f31c38f8743090ff66111898bb91dcc771e560cf
AdrianoRAV <adrianorodrigues38@gmail.com>
2023-11-21 11:02:23 -0300

    first commiit

--- desafio.dart
+++ desafio.dart
@@ -1,9 +1,7 @@
 void desafio(int n) {
-  // Inicializa a soma com 0.
   int soma = 0;
   int i = 0;
-  //n = 10;
-//Percorre todos os números inteiros de 1 a n-1.
+
   for (i = 0; i < n; i++) {
     if (i % 3 == 0 || i % 5 == 0) {
       soma += i;
@@ -11,10 +9,3 @@ void desafio(int n) {
   }
   print(" o valor é: $soma");
 }
-  //for i in range(1, n):
-  // Verifica se o número é divisível por 3 ou 5.
-   // if i % 3 == 0 or i % 5 == 0:
-    // Adiciona o número à soma.
-   //   soma += i
-  //Retorna a soma.
- //return soma
--- main.dart
+++ main.dart
@@ -1,13 +1,10 @@
 import 'dart:io';
-import 'desafioV2.dart';
+import 'desafio.dart';
 
 void main() {
   print('Digite um número inteio');
-  var input1 = stdin.readLineSync();
-  var inteiro = int.parse(input1!);
+  var input = stdin.readLineSync();
+  var inteiro = int.parse(input!);
 
-  soma_divisiveis_por_3_ou_5(inteiro);
-  // desafio(Resultado);
-
-  // calcularPorcentagens(totalEleitores, votosNulos, votosBrancos, votosValidos);*/
+  desafio(inteiro);
 }
